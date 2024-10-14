30c23c39a3fd9e44e7638261e30b4e2bf3f28ed7
dandnirv7 <dandnirv7@gmail.com>
2024-10-14 17:38:53 +0700

    style: update technology & thumbnail assets for projects

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="mb-10">
     <Head>
-      <Title>Dandnirv</Title>
+      <Title>Home | Dandi Nirpana Nahin</Title>
     </Head>
     <div
-      class="flex flex-col items-center justify-center space-y-10 md:items-start"
+      class="flex flex-col items-center justify-center gap-10 mt-10 md:items-start"
     >
       <div>
         <h1
           class="text-[2.5rem] md:text-[3rem] leading-none font-bold text-center md:text-start text-white"
         >
-          I'm <span class="text-[#64ffda]">Dandi</span>, a front-end developer
+          I'm <span class="text-tosca-green">Dandi</span>, a front-end developer
           with a passion for creating attractive, powerful and responsive web
           applications.
         </h1>
@@ -18,17 +18,17 @@
       <div class="w-max">
         <router-link
           to="/about-me"
-          class="flex items-center justify-center px-10 py-3 text-lg font-semibold text-white rounded-full bg-[#64ffda]"
+          class="flex items-center justify-center px-10 py-3 text-lg font-semibold text-white rounded-full bg-tosca-green/80"
         >
           Get to know me
         </router-link>
       </div>
     </div>
 
-    <div class="mt-40">
+    <div class="mt-32">
       <h1 class="text-3xl font-bold text-white">Things I Worked on.</h1>
       <div class="mt-10 mb-12">
-        <RandomProjects />
+        <ProjectsList :isRandom="true" />
       </div>
       <router-link
         to="/projects"
